35e29f37969acf1962f5128381744d212834e497
shashikdm <shashikant.123kadam@gmail.com>
2020-04-12 17:04:03 +0530

    revised scraper

---
+++ models/scrapper.js
@@ -0,0 +1,37 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+function _fetch(url, selector, profile) {
+    url = url+profile;
+    return new Promise(resolve => axios.get(url).then((response) => {
+            let $ = cheerio.load(response.data);
+            let rating = $(selector).toArray()[0].children[0].data;
+            resolve(rating);
+        }).catch((err) => {
+            throw err;
+        })
+    );
+}
+
+const platform = Object.freeze({
+    CODECHEF: 0,
+    CODEFORCES: 1,
+});
+
+const data = new Map();
+data.set(platform.CODECHEF, {
+    url: 'https://www.codechef.com/users/',
+    selector: '.rating-number'
+});
+data.set(platform.CODEFORCES, {
+    url: 'https://codeforces.com/profile/',
+    selector: 'span.user-green:nth-child(2)'
+});
+
+async function fetch(platformId, profile, callback) {
+    let platformData = data.get(platformId);
+    let rating = await _fetch(platformData.url, platformData.selector, profile);
+    callback(rating);
+}
+
+module.exports = {platform, fetch};
--- test.js
+++ test.js
@@ -1,4 +1,6 @@
-const {getCodeChefRating, getCodeForcesRating} = require('./models/utils')
+const scrapper = require('./models/scrapper')
+scrapper.fetch(scrapper.platform.CODEFORCES, 'shashikdm', console.log);
+scrapper.fetch(scrapper.platform.CODECHEF, 'shashikdm', console.log);
 
 // getCodeChefRating('shashikdm', console.log)
 // getCodeForcesRating('shashikdm', console.log)
